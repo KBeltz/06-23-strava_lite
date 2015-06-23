0f3c2a8b062c8f1ff3a868ead14b6765e6cef274
Kristin Beltz <beltz08@me.com>
2015-06-23 14:44:05 -0500

    Start building the database.

--- database_setup.rb
+++ database_setup.rb
@@ -1,10 +1,12 @@
 # Load/create our database for this program.
-CONNECTION = SQLite3::Database.new("strava_lite.db")
+DATABASE = SQLite3::Database.new("commuters.db")
 
 # Make the tables.
-CONNECTION.execute("CREATE TABLE IF NOT EXISTS cyclists (id INTEGER PRIMARY KEY, first_name TEXT NOT NULL, last_name TEXT NOT NULL, nickname TEXT);")
-CONNECTION.execute("CREATE TABLE IF NOT EXISTS bikes (id INTEGER PRIMARY KEY, FOREIGN KEY(cyclist_id) REFERENCES cyclists(id) NOT NULL, bike_name TEXT, description TEXT);")
-CONNECTION.execute("CREATE TABLE IF NOT EXISTS rides (id INTEGER PRIMARY KEY, FOREIGN KEY(cyclist_id) REFERENCES cyclists(id) NOT NULL, FOREIGN KEY(bike_id) REFERENCES bikes(id) NOT NULL, ride_name TEXT, miles, DECIMAL(5,2) NOT NULL, datetime CURRENT_TIMESTAMP NOT NULL);")
+DATABASE.execute("CREATE TABLE IF NOT EXISTS cyclists (id INTEGER PRIMARY KEY, first_name TEXT NOT NULL, last_name TEXT NOT NULL, nickname TEXT);")
+
+DATABASE.execute("CREATE TABLE IF NOT EXISTS bikes (id INTEGER PRIMARY KEY, cyclist_id INTEGER NOT NULL, bike_name TEXT, description TEXT);")
+
+DATABASE.execute("CREATE TABLE IF NOT EXISTS rides (id INTEGER PRIMARY KEY, cyclist_id INTEGER NOT NULL, bike_id INTEGER NOT NULL, ride_name TEXT, miles DECIMAL (5,2) NOT NULL, date DATETIME DEFAULT CURRENT_TIMESTAMP);")
 
 # Get results as an Array of Hashes.
-CONNECTION.results_as_hash = true
+DATABASE.results_as_hash = true
