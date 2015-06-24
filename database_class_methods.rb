7c63031b4248a8d10e928f02a6023026f944d742
Kristin Beltz <beltz08@me.com>
2015-06-24 11:41:32 -0500

    The database modules were copied over incorrectly. Replace them with the correct files and I will readjust as necessary if any issues arise.

--- database_class_methods.rb
+++ database_class_methods.rb
@@ -1,11 +1,104 @@
 require "active_support"
 require "active_support/inflector"
-
+ 
+ 
 # This module will be **extended** in all of my classes. It contains methods
 # that will become **class** methods for the class.
-
+ 
 module DatabaseClassMethods
   
+  # Get all of the rows for a table.
+  #
+  # Returns an Array containing Hashes for each row.
+  def all
+    # Figure out the table's name from the class we're calling the method on.
+    table_name = self.to_s.pluralize.underscore
+    
+    results = DATABASE.execute("SELECT * FROM #{table_name}")
+ 
+    results_as_objects = []
+    
+    results.each do |result_hash|
+      results_as_objects << self.new(result_hash)
+    end
+    
+    return results_as_objects
+  end
+  
+  # find - Use this method to find a 
+  # product based on the ID in database.
+  #
+  # record_id- The Integer ID of the row to return.
+  #
+  #
+  #    # Here, I'm using the `find` method from DatabaseClassMethods.
+  #    result = Product.find(product_id).first
+  #    # {"id" => 1, "name" => "Sumeet", "age" => 500}
+  #
+  #    
+  #  
+  # makes new instance of row
+  #
+  # Returns an object 
+  def find(record_id)
+    # Figure out the table's name from the class we're calling the method on.
+    table_name = self.to_s.pluralize.underscore
+    
+    result = DATABASE.execute("SELECT * FROM #{table_name} WHERE id = #{record_id}").first
+     
+    self.new(result)
+  end
+  
+  # sort - call to database to sort table by column name in descending order.
+  #
+  # Returns an Array of Objects
+  def sort(column_name)
+    table_name = self.to_s.pluralize.underscore
+    results = DATABASE.execute("SELECT * FROM #{table_name} ORDER BY #{column_name} DESC;")
+    
+    results_as_objects = []
+    
+    results.each do |result_hash|
+      results_as_objects << self.new(result_hash)
+    end
+    
+    return results_as_objects
+  end
+  
+  # sort - call to database to sort table by column name in ascending order.
+  #
+  # Returns an Array of Objects
+  def sort_asc(column_name)
+    table_name = self.to_s.pluralize.underscore
+    results = DATABASE.execute("SELECT * FROM #{table_name} ORDER BY #{column_name} ASC;")
+    
+    results_as_objects = []
+    
+    results.each do |result_hash|
+      results_as_objects << self.new(result_hash)
+    end
+    
+    return results_as_objects
+  end
+ 
+  # where - calls method on class to gather rows that have a specific value in a specific column.
+  # 
+  # Example Product.where("category_id", 3)
+  # => [234343 name: Chair brand: Spring category_id: 3 quantity: 4]
+  # 
+  # Returns an Array of Objects
+  def where(column_name, column_id)
+    table_name = self.to_s.pluralize.underscore
+    results = DATABASE.execute("SELECT * FROM #{table_name} WHERE #{column_name} = #{column_id};")
+    results_as_objects = []
+    
+    results.each do |result_hash|
+      results_as_objects << self.new(result_hash)
+    end
+    
+    return results_as_objects
+  end
+ 
   # Add a new record to the database.
   #
   # Returns an Object.
@@ -31,44 +124,13 @@ module DatabaseClassMethods
     
     # Alternative:
     # values.to_s.delete("\[\]")
-    
     table_name = self.to_s.pluralize.underscore
     
     DATABASE.execute("INSERT INTO #{table_name} (#{column_names_for_sql}) VALUES (#{values_for_sql});")
-
+ 
     id = DATABASE.last_insert_row_id
     options["id"] = id
-
+ 
     self.new(options)
-  end
-  
-  # Get all of the rows for a table.
-  #
-  # Returns an Array containing Objects for each row.
-  def all
-    # Figure out the table's name from the class we're calling the method on.
-    table_name = self.to_s.pluralize.underscore
-    
-    results = DATABASE.execute("SELECT * FROM #{table_name}")
-
-    results_as_objects = []
-    
-    results.each do |result_hash|
-      results_as_objects << self.new(result_hash)
-    end
-    
-    return results_as_objects
-  end
-  
-  # Get a single row.
-  #
-  # record_id - The record's Integer ID.
-  #
-  # Returns an Array containing the Hash of the row.
-  def find(record_id)
-    # Figure out the table's name from the class we're calling the method on.
-    table_name = self.to_s.pluralize.underscore
-    
-    DATABASE.execute("SELECT * FROM #{table_name} WHERE id = #{record_id}").first
   end
 end
--- database_instance_methods.rb
+++ database_instance_methods.rb
@@ -1,24 +1,50 @@
 require "active_support"
 require "active_support/inflector"
-
-# This module will be **included** in all of my classes. It contains methods
-# that will become **instance** methods in the class.
-
+ 
+ 
+# This module will be **extended** in all of my classes. It contains methods
+# that will become **class** methods for the class.
+ 
 module DatabaseInstanceMethods
-  
-  # Get the value of a field for a given row.
-  #
-  # field - String of the column name.
-  #
-  # Returns the String value of the cell in the table.
-  def get(field)
+ 
+  # delete - deleted entire row of object in table according to saved ID in instance.
+  # 
+  # Returns nil because row is deleted. 
+  # instance method.
+  def delete_row
     # Figure out the table's name from the object we're calling the method on.
     table_name = self.class.to_s.pluralize.underscore
-    
-    # Get the first/only row as a Hash.
-    result = DATABASE.execute("SELECT * FROM #{table_name} WHERE id = #{@id}").first
-    
-    # Return only the value for the key of the field we're seeking.
-    result[field]
+    DATABASE.execute("DELETE FROM #{table_name} WHERE id = #{@id};") # need to see if this one will work, if not look up.
+  end
+  
+  # Updates/saves a row's information in a table
+  #
+  # Returns updated Object
+  def save
+    table = self.class.to_s.pluralize.underscore
+ 
+    instance_variables = self.instance_variables
+ 
+    attribute_hash = {}
+ 
+    instance_variables.each do |variable|
+      attribute_hash["#{variable.slice(1..-1)}"] = self.send("#{variable.slice(1..-1)}")
+    end
+ 
+    individual_instance_variables = []
+ 
+    attribute_hash.each do |key, value|
+      if value.is_a?(String)
+        individual_instance_variables << "#{key} = '#{value}'"
+      else
+        individual_instance_variables << "#{key} = #{value}"
+      end
+    end
+ 
+    for_sql = individual_instance_variables.join(', ')
+ 
+    DATABASE.execute("UPDATE #{table} SET #{for_sql} WHERE id = #{self.id}")
+ 
+    return self
   end
 end
