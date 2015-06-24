35e87b221e9705ef7bbddb2497b970221a856a89
Kristin Beltz <beltz08@me.com>
2015-06-24 10:08:28 -0500

    Build the first model.

--- models/cyclist.rb
+++ models/cyclist.rb
@@ -1,5 +1,29 @@
+# require_relative "database_class_methods.rb"
+# require_relative "database_instance_methods.rb"
+
 class Cyclist
+  # extend DatabaseClassMethods
+  # include DatabaseInstanceMethods
+  
+
+  attr_accessor :id, :first_name, :last_name, :nickname
+
+  # Initializes a new Cyclist object
+  #
+  # id         - Integer id primary key created in the 'cyclists' table
+  # first_name - String containing the first name of the cyclist
+  # last_name  - String containing the last name of the cyclist
+  # nickname   - (optional) String containing the nickname of the cyclist
+  #
+  # Returns a Cyclist object
+  
   def initialize(options = {})
     
+    # Example: {"id" => 1, "first_name" => "Johnny", "last_name" => "Doe", "nickname" => "JohnBikes"}
+    @id = options["id"]
+    @first_name = options["first_name"]
+    @last_name = options["last_name"]
+    @nickname = options["nickname"]
+ 
   end
 end
