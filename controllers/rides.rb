73c5db2a22bb275f1d8d7d810d923f9be3150f1a
Kristin Beltz <beltz08@me.com>
2015-06-25 11:38:00 -0500

    Create Ride model.

---
+++ controllers/rides.rb
@@ -0,0 +1,89 @@
+# ---------------------------------------------------------------------
+# rides - menu
+# ---------------------------------------------------------------------
+
+# manage rides menu
+get "/rides" do
+  erb :"/rides/manage_rides"
+end
+
+# ---------------------------------------------------------------------
+# create
+# ---------------------------------------------------------------------
+
+# form for user to enter information to add ride
+get "/add_ride_form" do
+  erb :"/rides/add_ride_form"
+end
+
+# Gets parameters from add_ride_form.
+#
+# Adds ride to table
+get "/add_ride" do
+  if Ride.valid?(params["first_name"], params["last_name"])
+    erb :"/rides/add_ride_error"
+  else 
+    Ride.add_to_database("first_name" => params["first_name"], "last_name" => params["last_name"], "nickname" => params["nickname"])
+    erb :"/rides/success"
+  end
+end
+
+# ---------------------------------------------------------------------
+# read
+# ---------------------------------------------------------------------
+
+# Returns a list of all rows in the rides table.
+get "/view_rides" do
+  erb :"/rides/view_rides"
+end
+
+# form to enter ride id
+get "/search_by_id_form" do
+  erb :"/rides/search_by_id_form"
+end
+
+# Returns user profile
+get "/search_by_id/" do
+  @new_ride = Ride.find(params["id"])
+  erb :"/rides/search_by_id"
+end
+
+# ---------------------------------------------------------------------
+# update
+# ---------------------------------------------------------------------
+
+# table of ride profiles
+get "/edit_ride_list" do
+  erb :"/rides/edit_ride_list"
+end
+
+# edit rides form
+get "/edit_ride_form/:x" do
+  @new_ride = Ride.find(params["x"])
+  erb :"/rides/edit_ride_form"
+end
+
+# Gets parameters from edit_ride_form
+#
+# Updates the item in the appropriate table of the database
+get "/edit_ride/" do
+  @new_ride = Ride.find(params["x"])
+  @new_ride.save
+  
+  erb :"/rides/success"
+end
+
+# ---------------------------------------------------------------------
+# delete
+# ---------------------------------------------------------------------
+
+get "/delete_ride" do
+  erb :"/rides/delete_ride"
+end
+
+get "/delete_ride/:x" do
+  @specific_ride = Ride.find(params["x"])
+  @specific_ride.delete_row
+  
+  erb :"/rides/success"
+end
