a283379b8ec7c333af90ca6f4500aa7adcd16d76
Kristin Beltz <beltz08@me.com>
2015-06-28 15:18:33 -0500

    Removed utility module for now.

--- app.rb
+++ app.rb
@@ -2,9 +2,6 @@ require "pry"
 require "sinatra"
 require "sinatra/reloader"
 
-# Module
-require_relative "utility_module.rb"
-
 # SQL/Database
 require "sqlite3"
 require_relative "database_setup.rb"
